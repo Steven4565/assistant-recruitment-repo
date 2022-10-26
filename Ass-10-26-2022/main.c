ba26e32be20e0110d5a1bd5b66647c56b8ae5792
Steven4565 <stevenchristian4565@gmail.com>
2022-10-26 11:12:32 +0700

    10-26-2022 done

--- Ass-10-26-2022/main.c
+++ Ass-10-26-2022/main.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
+#define DEBUG 0
 
-int main() 
+int gcd(long long a, long long b)
+{
+    // base condition
+    if (a % b == 0)
+    {
+        if (DEBUG)
+            printf("gcd: %d\n", b);
+        return b;
+    }
+
+    if (DEBUG)
+    {
+        printf("a: %d ; b: %d ; a%%b: %d\n", a, b, a % b);
+    }
+
+    // recursion
+    return gcd(b, a % b);
+}
+
+int main()
 {
 
-    int test;
-    for (int t = 0; t < test; t++)
+    int n;
+    if (DEBUG)
+        n = 5;
+    else
+        scanf("%d", &n);
+
+    long long temp,
+        temp2;
+    scanf("%lld%lld", &temp, &temp2);
+    long long gcdResult = gcd(temp, temp2);
+
+    for (int t = 0; t < n - 2; t++)
     {
-        /* code */
+        long long input;
+        scanf("%lld", &input);
+
+        long long maxRes = (input > gcdResult ? input : gcdResult);
+        long long minRes = (input > gcdResult ? gcdResult : input);
+        gcdResult = gcd(maxRes, minRes);
     }
-    
+
+    printf("%lld\n", gcdResult);
 
     return 0;
 }
--- Ass-10-26-2022/test/test.c
+++ Ass-10-26-2022/test/test.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
-#define lambda(lambda$_ret, lambda$_args, lambda$_body)\
-({\
-lambda$_ret lambda$__anon$ lambda$_args\
-lambda$_body\
-&amp;lambda$__anon$;\
-})
+#define lambda(lambda$_ret, lambda$_args, lambda$_body) \
+    ({                                                  \
+        lambda$_ret lambda$__anon$ lambda$_args\
+lambda$_body        \
+            &amp;                                       \
+        lambda$__anon$;                                 \
+    })
 
-
-int add(int n, int y) {
+int add(int n, int y)
+{
     return n + 5;
 }
 
 // this is a function called functionFactory which receives parameter n
 // and returns a pointer to another function which receives two ints
 // and it returns another int
-int (*functionFactory(int n))(int, int) {
+int (*functionFactory(int n))(int, int)
+{
     printf("Got parameter %d", n);
-    int (*functionPtr)(int,int) = &add;
+    int (*functionPtr)(int, int) = &add;
     return functionPtr;
 }
-int main() {
+int main()
+{
 
     // int (*functionFactory(int n))(int, int)
-    int (*fn)(int, int) = functionFactory(5)(5,5);
+    int (*fn)(int, int) = functionFactory(5)(5, 5);
 
     return 0;
 }
